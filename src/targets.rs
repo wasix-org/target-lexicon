a5a360191e8e328c74b487ace6c9c58bb3fc94ef
Arshia Ghafoori <arshia001@live.com>
2025-06-26 16:07:58 +0400

    Add wasmer vendor, dl env for wasm32-wasmer-wasi-dl

--- src/targets.rs
+++ src/targets.rs
@@ -589,6 +589,7 @@ pub enum Vendor {
     Rumprun,
     Sun,
     Uwp,
+    Wasmer,
     Wrs,
 
     /// A custom vendor. "Custom" in this context means that the vendor is
@@ -621,6 +622,7 @@ impl Vendor {
             Rumprun => "rumprun",
             Sun => "sun",
             Uwp => "uwp",
+            Wasmer => "wasmer",
             Wrs => "wrs",
             Custom(name) => name.as_str(),
         }
@@ -798,6 +800,7 @@ pub enum Environment {
     AmdGiz,
     Android,
     Androideabi,
+    Dl,
     Eabi,
     Eabihf,
     Gnu,
@@ -841,6 +844,7 @@ impl Environment {
             AmdGiz => Cow::Borrowed("amdgiz"),
             Android => Cow::Borrowed("android"),
             Androideabi => Cow::Borrowed("androideabi"),
+            Dl => Cow::Borrowed("dl"),
             Eabi => Cow::Borrowed("eabi"),
             Eabihf => Cow::Borrowed("eabihf"),
             Gnu => Cow::Borrowed("gnu"),
@@ -1390,6 +1394,7 @@ impl FromStr for Vendor {
             "rumprun" => Rumprun,
             "sun" => Sun,
             "uwp" => Uwp,
+            "wasmer" => Wasmer,
             "wrs" => Wrs,
             custom => {
                 #[cfg(not(feature = "std"))]
@@ -1590,6 +1595,7 @@ impl FromStr for Environment {
             "amdgiz" => AmdGiz,
             "android" => Android,
             "androideabi" => Androideabi,
+            "dl" => Dl,
             "eabi" => Eabi,
             "eabihf" => Eabihf,
             "gnu" => Gnu,
